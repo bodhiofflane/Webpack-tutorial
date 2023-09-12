c7b996f46d59cba5ca2c2e2d73c3310664b25331
bodhiofflane <channelbodhi@gmail.com>
2023-09-12 06:11:59 +0300

    CommonConfig

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,25 +1,11 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
 const MiniCssExrtactPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
+module.exports = merge(common, {
   mode: 'production',
-  entry: './src/index.js',
-  devServer: {
-    static: './dist', // Сообщяем серверу откуда сервить файлы, не собитая в ./dist.
-  },
   module: {
     rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
       {
         test: /\.scss$/i,
         use: [
@@ -29,20 +15,13 @@ module.exports = {
           'css-loader',
           'postcss-loader',
           'sass-loader',
-        ]
+        ],
       },
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development',
-    }),
     new MiniCssExrtactPlugin({
       filename: '[name].css',
-    })
+    }),
   ],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
+});
